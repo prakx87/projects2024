3cf38eefab0ed7be7e2b3f6d702b44d2c53432d7
prakx87 <prakx_rj@yahoo.co.in>
2023-12-31 00:20:18 +0530

    Learning generators

--- asynchrony/theory.py
+++ asynchrony/theory.py
@@ -1,2 +1,12 @@
 def odds(start, stop):
-    
+    for odd in range(start, stop + 1, 2):
+        yield odd
+
+def main():
+    odd_values = [odd for odd in odds(3, 15)]
+    odds2 = tuple(odds(21, 29))
+    print(odd_values)
+    print(odds2)
+
+if __name__ == "__main__":
+    main()

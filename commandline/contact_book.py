c26172ea0d530c413c682da0f70aca3e4ce7065e
prakx87 <prakx_rj@yahoo.co.in>
2023-12-30 18:36:43 +0530

    Added backup option

--- commandline/contact_book.py
+++ commandline/contact_book.py
@@ -15,6 +15,7 @@ User levels
 import sqlite3
 import argparse
 import sys
+from datetime import datetime
 
 
 class ContactBook():
@@ -27,11 +28,11 @@ class ContactBook():
         )
         parser.add_argument(
             "operation",
-            choices=["create", "update", "delete", "search"],
+            choices=["create", "update", "delete", "search", "backup"],
             help="Specify what kind of operation you wish to do: CRUD/SEARCH"
         )
 
-        parser.add_argument("-n", "--name", type=str, required=True)
+        parser.add_argument("-n", "--name", type=str, required=False)
         parser.add_argument("-e", "--email", type=str, required=False, default="")
         parser.add_argument("-a", "--address", type=str, required=False, default="")
         parser.add_argument("-p", "--phone", type=int, required=False, default=0)
@@ -40,17 +41,17 @@ class ContactBook():
 
 
     # function for CRUD operations
-    def contact_book_create(self, dbconn, details):
+    def contact_book_create(self):
         query = f"INSERT INTO CONTACT (NAME,EMAIL,ADDRESS,PHONE) \
-            VALUES ('{details['name']}','{details['email']}','{details['address']}',{details['phone']})"
-        dbconn.execute(query)
-        dbconn.commit()
-        print(dbconn.total_changes)
+            VALUES ('{self.details['name']}','{self.details['email']}','{self.details['address']}',{self.details['phone']})"
+        self.conn.execute(query)
+        self.conn.commit()
+        print(self.conn.total_changes)
 
 
-    def contact_book_update(self, dbconn, details):
+    def contact_book_update(self):
         # search for contact from DB
-        query_output = contact_book_search(dbconn, details)
+        query_output = self.contact_book_search()
         if len(query_output) > 1:
             print("Error: More than 1 Contact returned. Need more info to update contact.")
             sys.exit(1)
@@ -62,13 +63,13 @@ class ContactBook():
         print("Click Enter without typing if you wish keep previous value in the field.")
         name = input("Name: ")
         if name is None:
-            name = details["name"]
+            name = self.details["name"]
         email = input("Email: ")
         if email is None:
-            email = details["email"]
+            email = self.details["email"]
         phone = input("Phone: ")
         if phone is None:
-            phone = details["phone"]
+            phone = self.details["phone"]
         else:
             try:
                 phone = int(phone)
@@ -78,13 +79,13 @@ class ContactBook():
         address = input("Address: ")
 
         query = f"UPDATE CONTACT set NAME = '{name}', EMAIL = '{email}', PHONE = {phone}, ADDRESS = '{address}';"
-        dbconn.execute(query)
-        dbconn.commit()
-        print(dbconn.total_changes)
+        self.conn.execute(query)
+        self.conn.commit()
+        print(self.conn.total_changes)
 
 
-    def contact_book_delete(self, dbconn, details):
-        query_output = contact_book_search(dbconn, details)
+    def contact_book_delete(self):
+        query_output = self.contact_book_search()
         print_contact(query_output)
 
         delete_status = input("Do you wish to delete these contacts: Y/N ? ")
@@ -96,16 +97,16 @@ class ContactBook():
             queries.append(f"DELETE FROM CONTACT WHERE ID = {contact[0]};")
 
         for query in queries:
-            dbconn.execute(query)
-            dbconn.commit()
-            print(dbconn.total_changes)
+            self.conn.execute(query)
+            self.conn.commit()
+            print(self.conn.total_changes)
 
 
     # function for Search operations
-    def contact_book_search(self, dbconn, details):
-        query = f"SELECT * FROM CONTACT WHERE NAME = '{details['name']}'"
+    def contact_book_search(self):
+        query = f"SELECT * FROM CONTACT WHERE NAME = '{self.details['name']}'"
 
-        for key, val in details.items():
+        for key, val in self.details.items():
             if key != "name" and val not in ["", 0]:
                 query += f" AND {key.upper()}"
                 if type(val) == str:
@@ -115,7 +116,7 @@ class ContactBook():
 
         print(query + ";")
 
-        cursor = dbconn.execute(query)
+        cursor = self.conn.execute(query)
         return cursor.fetchall()
 
 
@@ -130,8 +131,8 @@ class ContactBook():
 
     # connect to DB
     def db_connection(self):
-        conn = sqlite3.connect("contact_book.db")
-        conn.execute(
+        self.conn = sqlite3.connect("contact_book.db")
+        self.conn.execute(
             """
             CREATE TABLE IF NOT EXISTS CONTACT
             (ID     INTEGER PRIMARY KEY,
@@ -141,32 +142,46 @@ class ContactBook():
             ADDRESS CHAR(50))
             """
         )
-        conn.commit()
-        return conn
+        self.conn.commit()
+
+    def progress(self, status, remaining, total):
+        print(f'Copied {total-remaining} of {total} pages...')
+
+
+    def take_db_back(self):
+        time_now = datetime.now().strftime("%Y-%m-%d")
+        backup_name = f"./backup_book_{time_now}.db"
+        print(backup_name)
+        dest = sqlite3.connect(backup_name)
+        with dest:
+            self.conn.backup(dest, progress=self.progress)
+        dest.close()
 
 
     def main(self):
         arguments = self.get_arguments()
         # print(f"Operation Type: {arguments}")
 
-        details = {
+        self.details = {
             "name": arguments.name,
             "email": arguments.email,
             "address": arguments.address,
             "phone": arguments.phone,
         }
 
-        dbconn = db_connection()
+        self.db_connection()
 
         if arguments.operation == "create":
-            contact_book_create(dbconn, details)
+            self.contact_book_create()
         elif arguments.operation == "update":
-            contact_book_update(dbconn, details)
+            self.contact_book_update()
         elif arguments.operation == "delete":
-            contact_book_delete(dbconn, details)
+            self.contact_book_delete()
+        elif arguments.operation == "backup":
+            self.take_db_back()
         else:
-            query_output = contact_book_search(dbconn, details)
-            print_contact(query_output)
+            query_output = self.contact_book_search()
+            self.print_contact(query_output)
 
 
 if __name__ == "__main__":

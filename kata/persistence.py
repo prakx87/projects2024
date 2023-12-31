d82bccbd94eef39c98ab85ae3f52573f3aa256f9
prakx87 <prakx_rj@yahoo.co.in>
2023-12-31 22:55:48 +0530

    Optimized persistence kata

--- kata/persistence.py
+++ kata/persistence.py
@@ -1,11 +1,9 @@
 def persistence(n, count = 0):
     if len(str(n)) == 1:
         return count
-    else:
-        count += 1
 
     i = 1
     for j in str(n):
         i *= int(j)
 
-    return persistence(i, count)
+    return persistence(i, count + 1)
